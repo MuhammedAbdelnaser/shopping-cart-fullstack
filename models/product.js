0738fc1120e628b3248be9e4569ea44b2df4b057
MuhammedAbdelnaser <mohamedelreety2014@gmail.com>
2024-06-27 02:42:13 +0300

    update models

--- models/product.js
+++ models/product.js
@@ -1,18 +1,42 @@
-const products = [];
+const fs = require('fs');
+const path = require('path');
 
-module.exports = class Product{
+const p = path.join(
+  path.dirname(require.main.filename),
+  'data',
+  'products.json'
+)
+
+const getProductsFromFile = cb => {
+  return fs.readFile(p, (err, fileContent) => {
+    if (err) {
+      cb([]);
+    } else {
+      cb(JSON.parse(fileContent))
+    }
+  })
+}
+
+module.exports = class Product {
   
-  constructor(title, price, description) {
+  constructor(title, imageUrl, description, price) {
     this.title = title
-    this.price = price
+    this.imageUrl = imageUrl
     this.description = description
+    this.price = price
   }
 
   save() {
-    products.push(this);
+    getProductsFromFile(products => {
+      products.push(this)
+      fs.writeFile(p, JSON.stringify(products), (err) => {
+        console.log(err)
+      });
+    })
+
   }
 
-  static fetchAll() {
-    return products
+  static fetchAll(cb) {
+    getProductsFromFile(cb)
   }
 };
