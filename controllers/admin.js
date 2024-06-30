1fdcd056e307457011cb2c562134d8dc80b347a1
MuhammedAbdelnaser <mohamedelreety2014@gmail.com>
2024-06-30 21:26:46 +0300

    update controllers and add cart

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,22 +1,64 @@
 const Product = require('../models/product');
 
 exports.getAddProduct = (req, res, next) => {
-  res.render("admin/add-product", {
+  res.render("admin/edit-product", {
     pageTitle: "Add Product",
     path: "/admin/add-product",
-    formsCSS: true,
-    productCSS: true,
-    activeAddProduct: true
+    editing: false
   })
 }
 
 exports.postAddProduct = (req, res, next) => {
   const {title, imageUrl, price, description } = req.body
-  const product = new Product(title, imageUrl, description, price)
-
+  const product = new Product(null, title, imageUrl, description, price)
+  
   product.save()
   res.redirect('/')
 }
+
+exports.getEditProduct = (req, res, next) => {
+  const editMode = req.query.edit;
+  if (!editMode) {
+    return res.redirect('/')
+  }
+  const { productId } = req.params
+  Product.findById(productId, product => {
+    if (!product) {
+      // Not the best UX as you should show the error to the user 
+      return res.redirect('/')
+    }
+    res.render("admin/edit-product", {
+      pageTitle: "Edit Product",
+      path: "/admin/edit-product",
+      product,
+      editing: editMode
+    })
+  })
+}
+
+exports.postEditProduct = (req, res, next) => {
+  const { productId, title, imageUrl, description, price } = req.body
+
+  const updatedProduct = new Product(
+    productId,
+    title,
+    imageUrl,
+    description,
+    price
+  )
+
+  updatedProduct.save()
+
+  res.redirect('/admin/products')
+
+}
+
+exports.deleteProduct = (req, res, next) => {
+  const { productId } = req.body
+  Product.deleteById(productId)
+  res.redirect('/admin/products')
+}
+
 exports.getProducts = (req, res, next) => {
   const products = Product.fetchAll((products) => {
     res.render("admin/products", {
--- controllers/shop.js
+++ controllers/shop.js
@@ -1,4 +1,6 @@
 const Product = require('../models/product');
+const Cart = require('../models/cart');
+
 
 exports.getProducts = (req, res, next) => {
   const products = Product.fetchAll((products) => {
@@ -23,11 +25,43 @@ exports.getIndex = (req, res, next) => {
 }
 
 exports.getCart = (req, res, next) => {
-  res.render("shop/cart", {
-    path: '/cart',
-    pageTitle: 'Your Cart'
+  Cart.getProducts(cart => {
+    Product.fetchAll(products => {
+      const cartProducts = [];
+      for (const product of products) {
+        const cartProductData = cart.products.find(prod => prod.id === product.id)
+        if (cartProductData) {
+          cartProducts.push({product, quantity: cartProductData.quantity, totalPrice: product.price * cartProductData.quantity })
+        }
+      }
+      console.log('cartProducts', cartProducts)
+      res.render("shop/cart", {
+        path: '/cart',
+        pageTitle: 'Your Cart',
+        products: cartProducts
+      })
+    })
   })
 }
+
+exports.postCart = (req, res, next) => {
+  const { productId } = req.body;
+  Product.findById(productId, (product) => {
+    Cart.addProduct(productId, product.price)
+  })
+
+  res.redirect('/cart')
+}
+
+exports.postDeleteCartItem = (req, res, next) => {
+  const { productId } = req.body;
+  Product.findById(productId, (product) => {
+    Cart.deleteProduct(productId, product.price)
+    res.redirect('/cart')
+  })
+
+}
+
 exports.getOrders = (req, res, next) => {
   res.render("shop/orders", {
     path: '/orders',
