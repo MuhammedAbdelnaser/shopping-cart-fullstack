e5ace7245e25486e1c5e5594d696ff0add8f710c
MuhammedAbdelnaser <mohamedelreety2014@gmail.com>
2024-09-03 02:04:52 +0300

    create admin controller

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,4 +1,6 @@
+const mongodb = require('mongodb');
 const Product = require('../models/product');
+
 
 exports.getAddProduct = (req, res, next) => {
   res.render("admin/edit-product", {
@@ -9,13 +11,16 @@ exports.getAddProduct = (req, res, next) => {
 }
 
 exports.postAddProduct = (req, res, next) => {
-  const {title, imageUrl, price, description } = req.body
-  req.user.createProduct({
+  const {title, price, description, imageUrl} = req.body
+  const product = new Product(
     title,
-    imageUrl,
     price,
     description,
-  }).then( () => {
+    imageUrl,
+    null,
+    req.user._id
+  );
+  product.save().then( () => {
     console.log('Product Created Successfully')
     res.redirect('/')
   }).catch( err => {
@@ -30,11 +35,7 @@ exports.getEditProduct = (req, res, next) => {
   }
   const { productId } = req.params
 
-  req.user
-  .getProducts({
-    where: { id: productId }
-  }).then(products => {
-    const product = products[0]
+  Product.findById(productId).then(product => {
     if (!product) {
       // Not the best UX as you should show the error to the user 
       return res.redirect('/')
@@ -51,38 +52,25 @@ exports.getEditProduct = (req, res, next) => {
 
 exports.postEditProduct = async (req, res, next) => {
   const { productId, title, imageUrl, description, price } = req.body
-
-  try {
-    const product = await Product.findByPk(productId);
-
-    if (!product) {
-      // Handle the case where the product does not exist
-      console.log('Product not found');
-      return res.redirect('/');
-    }
-
-    product.title = title;
-    product.imageUrl = imageUrl;
-    product.description = description;
-    product.price = price;
-
-    await product.save()
-    .then(() => {
-      res.redirect('/admin/products')
-      console.log('PRODUCT UPDATED SUCCESSFULLY')
-    })
-  } catch (err) {
-    console.log('Error Editing Product', err)
-  }
-
+    const updatedTitle = title;
+    const updatedPrice = price;
+    const updatedDescription = description;
+    const updatedImageUrl = imageUrl;
+    
+    const product = new Product(updatedTitle, updatedPrice, updatedDescription, updatedImageUrl, productId);
+    return product.save()
+      .then(() => {
+        res.redirect('/admin/products')
+        console.log('PRODUCT UPDATED SUCCESSFULLY')
+      })
+      .catch(err => {
+        console.log('Error Updating Product', err)
+      })
 }
 
 exports.deleteProduct = (req, res, next) => {
   const productId = req.body.productId
-  Product.findByPk(productId)
-  .then(product => {
-    return product.destroy()
-  })
+  Product.deleteById(productId)
   .then(() => {
     console.log('PRODUCT DELETED SUCCESSFULLY')
     res.redirect('/admin/products')
@@ -92,9 +80,7 @@ exports.deleteProduct = (req, res, next) => {
 }
 
 exports.getProducts = (req, res, next) => {
-  
-  req.user
-  .getProducts().then(products => {
+  Product.fetchAll().then(products => {
     res.render("admin/products", {
       pageTitle: "Products page for Admin",
       path: '/admin/products',
