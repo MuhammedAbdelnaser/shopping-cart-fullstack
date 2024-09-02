ee5a000b8c26531f6a65fce8b18efc9b5f98f3dc
MuhammedAbdelnaser <mohamedelreety2014@gmail.com>
2024-09-03 02:08:19 +0300

    update database using mongo

--- utils/database.js
+++ utils/database.js
@@ -2,15 +2,28 @@ const mongodb = require('mongodb');
 const mongoClient = mongodb.MongoClient
 require('dotenv').config();
 
+let _db;
+
 const mongoConnect = callback => {
   mongoClient.connect(process.env.MONGODB_URI)
     .then(client => {
-      console.log("CONNECTED!")
-      callback(client)
+      console.log("CONNECTED!");
+      _db = client.db();
+      callback();
     })
     .catch(err => {
       console.log('Error starting the server', err)
+      throw err
     })
 }
 
-module.exports = mongoConnect
+const getDB = () => {
+  if(_db) {
+    return _db;
+  }
+  throw 'No Database Found'
+
+}
+
+exports.mongoConnect = mongoConnect
+exports.getDB = getDB
