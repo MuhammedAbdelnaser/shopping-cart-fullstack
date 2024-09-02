2571673612ceaba7307df0ccc37121a1f96181af
MuhammedAbdelnaser <mohamedelreety2014@gmail.com>
2024-09-03 02:04:29 +0300

    connect to mongo db with a specific user

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const bodyParser = require('body-parser');
 const path = require('path');
 
 const errorController = require('./controllers/error');
-const mongoConnect = require('./utils/database');
+const mongoConnect = require('./utils/database').mongoConnect;
+const User = require('./models/user');
 
 const app = express();
 
@@ -11,21 +12,26 @@ app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
-// const shopRoutes = require('./routes/shop');
+const shopRoutes = require('./routes/shop');
 
 app.use((req, res, next) => {
-  
+  User.findById('66ce61e03050410b3fd28af4')
+  .then(user => {
+    req.user = new User(user.name, user.email, user.cart, user._id)
+    next()
+  }).catch((err) => {
+    console.log('Error Find User', err)
+  });
 })
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/admin', adminRoutes);
-// app.use(shopRoutes);
+app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-mongoConnect((client) => {
-  console.log('client', client)
+mongoConnect(() => {
   app.listen(3000)
 })
