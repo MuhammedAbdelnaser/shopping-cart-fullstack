ee55bba5bd661732ee0de6a9e7f616c897eacfbd
MuhammedAbdelnaser <mohamedelreety2014@gmail.com>
2024-09-19 13:49:09 +0300

    add route validation

--- routes/admin.js
+++ routes/admin.js
@@ -2,23 +2,46 @@ const path = require('path');
 
 const express = require('express');
 
+const { body } = require('express-validator');
+
 const adminController = require('../controllers/admin');
+const isAuth = require('../middleware/is-auth');
 
 const router = express.Router();
 
 // /admin/products => GET
-router.get('/products', adminController.getProducts)
+router.get('/products', isAuth, adminController.getProducts)
 
 // /admin/add-product => POST
-router.post('/add-product', adminController.postAddProduct)
+router.post(
+  '/add-product',
+  isAuth,
+  [
+    body('title', 'Invalid product title, it must be at least 6 chars').isString().isLength({ min: 6 }).trim(),
+    body('price', 'Price must be Number').isFloat(),
+    body('description', 'description must be at least 20 chars and at most 400 chars').isString().isLength({ min: 20, max: 400 }).trim(),
+    body('imageUrl', 'invalid image URL').isURL(),
+  ],
+  adminController.postAddProduct
+)
 
 // /admin/add-product => GET
-router.get('/add-product', adminController.getAddProduct)
+router.get('/add-product', isAuth, adminController.getAddProduct)
 
-router.get('/edit-product/:productId', adminController.getEditProduct)
+router.get('/edit-product/:productId', isAuth, adminController.getEditProduct)
 
-router.post('/edit-product/', adminController.postEditProduct)
+router.post(
+  '/edit-product/', 
+  [
+    body('title', 'Invalid product title, it must be at least 6 chars').isString().isLength({ min: 6 }).trim(),
+    body('price', 'Price must be Number').isFloat(),
+    body('description', 'description must be at least 20 chars and at most 400 chars').isString().isLength({ min: 20, max: 400 }).trim(),
+    body('imageUrl', 'invalid image URL').isURL(),
+  ],
+  isAuth,
+  adminController.postEditProduct
+)
 
-router.post('/delete-product', adminController.deleteProduct)
+router.post('/delete-product', isAuth, adminController.deleteProduct)
 
 module.exports = router
