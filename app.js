407431dd9b19335ff929cdac6a9258c4ad24eed1
MuhammedAbdelnaser <mohamedelreety2014@gmail.com>
2024-09-19 13:32:58 +0300

    using csurf in the app

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const path = require('path');
 const mongoose = require('mongoose');
 const session = require('express-session');
 const MongoDBStore = require('connect-mongodb-session')(session);
+const csrf = require('csurf');
+const flash = require('connect-flash');
 require('dotenv').config();
 
 const errorController = require('./controllers/error');
@@ -13,6 +15,9 @@ const store = new MongoDBStore({
   uri: process.env.MONGODB_URI,
   collection: 'sessions',
 });
+
+const csrfProtection = csrf();
+app.use(flash())
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -32,41 +37,46 @@ app.use(session({
   store
 }))
 
+app.use(csrfProtection);
+
 app.use((req, res, next) => {
   if(!req.session.user) {
-    next()
+    return next()
   }
   User.findById(req.session.user._id)
   .then(user => {
+    if (!user) {
+      return next()
+    }
     req.user = user;
     next()
   })
   .catch((err) => {
-    console.log('Error Find User', err)
+    throw new Error(err);
+    
   });
 })
 
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.session.isLoggedIn;
+  res.locals.csrfToken = req.csrfToken();
+  next()
+})
 
-app.use('/admin', adminRoutes);
+app.use('/admin',   adminRoutes);
 app.use(shopRoutes);
 app.use(authRoutes);
 
+// app.use(errorController.get500);
+
 app.use(errorController.get404);
+
+app.use((error, req, res, next) => {
+  res.redirect('/500')
+})
 
 mongoose.connect(process.env.MONGODB_URI)
 .then(() => {
-  User.findOne().then(users => {
-    if (!users) {
-      const user = new User({
-        name: 'Muhammed',
-        email: 'Muhammed@me.com',
-        cart: {
-          items: []
-        }
-      })
-      user.save();
-    }
-  })
   app.listen(3000)
 }).catch((err) => {
   console.log('ERROR CONNECTING', err)
