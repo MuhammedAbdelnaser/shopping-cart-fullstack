78b6f099dd2db4bcbbe29231e2b79f511dcdd62f
MuhammedAbdelnaser <mohamedelreety2014@gmail.com>
2024-09-19 13:35:25 +0300

    use express validator

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,19 +1,42 @@
-const mongodb = require('mongodb');
 const Product = require('../models/product');
+const { validationResult } = require('express-validator');
 
 
 exports.getAddProduct = (req, res, next) => {
+  if (!req.session.isLoggedIn) {
+    return res.redirect('/')
+  }
   res.render("admin/edit-product", {
     pageTitle: "Add Product",
     path: "/admin/add-product",
     editing: false,
-    isAuthenticated: req.session.isLoggedIn
+    hasError: false,
+    errorMessage: null,
+    product: [],
+    validationErrors: []
   })
 }
 
 exports.postAddProduct = (req, res, next) => {
   const {title, price, description, imageUrl} = req.body
-  console.log('req.user', req.user)
+  const errors = validationResult(req)
+
+  if (!errors.isEmpty()) {
+    return res.status(422).render("admin/edit-product", {
+      pageTitle: "Edit Product",
+      path: "/admin/edit-product",
+      editing: false,
+      hasError: true,
+      product: {
+        title,
+        price,
+        description,
+        imageUrl,
+      },
+      errorMessage: errors.array()[0].msg
+    })
+  }
+
   const product = new Product({
     title,
     price,
@@ -27,7 +50,9 @@ exports.postAddProduct = (req, res, next) => {
     console.log('Product Created Successfully')
     res.redirect('/')
   }).catch( err => {
-    console.log('Error Creating Product', err)
+    const error = new Error(err);
+    error.httpStatusCode = 500
+    return next(error)
   })
 }
 
@@ -48,50 +73,87 @@ exports.getEditProduct = (req, res, next) => {
       path: "/admin/edit-product",
       product,
       editing: editMode,
-      isAuthenticated: req.session.isLoggedIn
+      hasError: false,
+      errorMessage: null,
+      validationErrors: []
     })
+  }).catch(() => {
+      const error = new Error(err);
+      error.httpStatusCode = 500
+      return next(error)
   })
-
+  
 }
 
 exports.postEditProduct = async (req, res, next) => {
   const { productId, title, imageUrl, description, price } = req.body
+  const errors = validationResult(req)
+  
+  if (!errors.isEmpty()) {
+    return res.status(422).render("admin/edit-product", {
+      pageTitle: "Edit Product",
+      path: "/admin/edit-product",
+      editing: true,
+      hasError: true,
+      product: {
+        _id: productId,
+        title,
+        imageUrl,
+        description,
+        price
+      },
+      errorMessage: errors.array()[0].msg,
+      validationErrors: errors.array()
+    })
+    
+  }
+
   Product.findById(productId)
     .then(product => {
+      if(product.userId.toString() !== req.user._id.toString()) {
+        return res.redirect('/');
+      }
       product.title = title;
       product.price = price;
       product.description = description;
       product.imageUrl = imageUrl;
       return product.save()
+      .then(() => {
+        console.log('PRODUCT UPDATED SUCCESSFULLY')
+        res.redirect('/admin/products')
+      })
     })
-    .then(() => {
-      console.log('PRODUCT UPDATED SUCCESSFULLY')
-      res.redirect('/admin/products')
-    })
-    .catch(err => {
-      console.log('Error Updating Product', err)
+    .catch(() => {
+      const error = new Error(err);
+      error.httpStatusCode = 500
+      return next(error)
     })
 }
 
 exports.deleteProduct = (req, res, next) => {
   const productId = req.body.productId
-  Product.findByIdAndDelete(productId)
+  Product.deleteOne({
+    _id: productId,
+    userId: req.user._id
+  })
   .then(() => {
     console.log('PRODUCT DELETED SUCCESSFULLY')
     res.redirect('/admin/products')
   }).catch(err => {
-    console.log('Error Deleting Product', err);
+    const error = new Error(err);
+    error.httpStatusCode = 500
+    return next(error)
   })
 }
 
 exports.getProducts = (req, res, next) => {
-  Product.find()
+  Product.find({ userId: req.user._id })
   .then(products => {
     res.render("admin/products", {
       pageTitle: "Products page for Admin",
       path: '/admin/products',
       prods: products,
-      isAuthenticated: req.session.isLoggedIn
+      
     })
   }).catch(err => {
     console.log('Error loading Admin Products', err)
